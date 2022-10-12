6106706d491dd248f842e91e5ef49318ccc1516e
yuhe <dingyh94@yahoo.com>
2022-10-12 22:55:03 +0800

    feat: webpack相关

---
+++ 17.webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  // 入口
+  entry: "./src/index.js",
+  module: {
+    rules: [{ test: /\.txt/, use: "raw-loader" }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
