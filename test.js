9ea2b844e2afb37782f5bc7b6a42f1b40f5a5a6c
yuhe <dingyh94@yahoo.com>
2022-10-18 10:13:41 +0800

    feat: curry

--- test.js
+++ test.js
@@ -14,15 +14,38 @@
 // console.log(a);
 // let a;
 
-var a = {
-  'one':1,
-  'two':2,
+// var a = {
+//   'one':1,
+//   'two':2,
+// }
+
+// function fn(b){
+//   console.log(a === b)
+// }
+
+// fn(a)
+// console.log(a)
+
+class UserModel {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  say() {
+    return this.firstname + this.lastname;
+  }
 }
 
-function fn(b){
-  console.log(a === b)
+const user = new UserModel("h", "z");
+
+function shallowCopy(user) {
+  let resArgs = [];
+  for (let key in user) {
+    resArgs.push(user[key]);
+  }
+  let res = new user.constructor(...resArgs);
+  return res;
 }
 
-fn(a)
-console.log(a)
-
+const copy = shallowCopy(user);
+console.log(copy);
