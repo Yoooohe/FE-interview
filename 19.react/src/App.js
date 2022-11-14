753250ffe4e2938643cfa0605896de246451e5a7
yuhe <dingyh94@yahoo.com>
2022-11-14 20:07:25 +0800

    feat: setState异步

--- 19.react/src/App.js
+++ 19.react/src/App.js
@@ -2,16 +2,38 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setCount(count + 1);
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(id);
+  //   };
+  // }, [count]);
+
+  const [number, setNumber] = useState(0);
+  console.log("????", number);
+
   useEffect(() => {
-    const id = setInterval(() => {
-      setCount(count + 1);
-    }, 1000);
-    return () => {
-      clearInterval(id);
-    };
-  }, [count]);
-  return <div>{count}</div>;
+    setNumber((num) => {
+      console.log("---0---: ", num);
+      return num + 1;
+    });
+    console.log("----0000");
+    setNumber((num) => {
+      console.log("---1---: ", num);
+      return num + 1;
+    });
+    console.log("----1111");
+    setNumber((num) => {
+      console.log("---2---: ", num);
+      return num + 1;
+    });
+    console.log("----2222");
+  }, []);
+
+  return <div>{number}</div>;
 }
 
 export default App;
